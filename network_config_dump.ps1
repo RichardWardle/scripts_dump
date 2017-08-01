abbd1c3b89d7fb445bee1b652c033cd7a3229cb2
RichardWardle <wardle.rich@gmail.com>
2017-08-01 11:43:30 +1000

    Create network_config_dump.ps1

---
+++ network_config_dump.ps1
@@ -0,0 +1,26 @@
+# You can hard code your list of servers if its easier
+#$computers = "dc.example.com","member1.example.com"
+
+# I provided my script with a file that had my servers in a list
+$computers  = Get-content C:\computers.txt
+
+$networks = @()
+
+foreach ($ind in $computers)
+{
+    
+    try
+    {
+        $Networks += Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $ind -EA SilentlyContinue | select -property DNSHostName, IPAddress, IPSubnet, MACAddress, DefaultIPGateway, DHCPEnabled | ? {$_.IPAddress}
+    }
+    catch
+    {
+        Write-Warning "I could not connect to $ind, please investigate this manually"
+    }
+}
+#Super lazy here outputting to csv gave me System.String[] since you could have multiple IP's or subnets on a nic
+#to avoid this problem i just output to table then into a file in txt and manipulate it in excel
+$networks | ft > C:\subnet_results.txt
+
+# Output results to console at the end
+$networks 
