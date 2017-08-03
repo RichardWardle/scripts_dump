f34877d1720cf2b411bb37bba4f6aa8724a17ae1
RichardWardle <wardle.rich@gmail.com>
2017-08-03 20:40:36 +1000

    Update network_config_dump.ps1

--- network_config_dump.ps1
+++ network_config_dump.ps1
@@ -4,26 +4,50 @@
 # I provided my script with a file that had my servers in a list
 $computers  = Get-content C:\computers.txt
 
-$networks = @()
+$EndindResult = @()
 
 foreach ($ind in $computers)
 {
   if (Test-WSMan $ind -ea SilentlyContinue) {
         try
         {
-            $Networks += Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $ind -EA SilentlyContinue | select -property DNSHostName, IPAddress, IPSubnet, MACAddress, DefaultIPGateway, DHCPEnabled | ? {$_.IPAddress}
+            $Networks = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $ind -EA SilentlyContinue | ? {$_.IPEnabled}    
         }
         catch
         {
             Write-Warning "I could not connect to $ind, please investigate this manually even though Test-WSMan passed"
         }
+    
+    foreach ($Network in $Networks)
+    {            
+        
+        $IPAddress  = $Network.IpAddress[0]            
+        $SubnetMask  = $Network.IPSubnet[0]            
+        $DefaultGateway = $Network.DefaultIPGateway            
+        $DNSServers  = $Network.DNSServerSearchOrder            
+        $MACAddress  = $Network.MACAddress 
+        $Description  = $Network.Description
+        $SuffixRegistration = $Network.DomainDNSRegistrationEnabled 
+        $SuffixSearchOrder = $Network.DNSDomainSuffixSearchOrder       
+        If($network.DHCPEnabled) { $IsDHCPEnabled = $true } else { $IsDHCPEnabled = $false }
+                     
+        $indResult  = New-Object -Type PSObject            
+        $indResult | Add-Member -MemberType NoteProperty -Name ComputerName -Value $ind.ToUpper()            
+        $indResult | Add-Member -MemberType NoteProperty -Name IPAddress -Value $IPAddress            
+        $indResult | Add-Member -MemberType NoteProperty -Name SubnetMask -Value $SubnetMask            
+        $indResult | Add-Member -MemberType NoteProperty -Name Gateway -Value $DefaultGateway            
+        $indResult | Add-Member -MemberType NoteProperty -Name IsDHCPEnabled -Value $IsDHCPEnabled            
+        $indResult | Add-Member -MemberType NoteProperty -Name DNSServers -Value $DNSServers            
+        $indResult | Add-Member -MemberType NoteProperty -Name MACAddress -Value $MACAddress 
+        $indResult | Add-Member -MemberType NoteProperty -Name SuffixSearchOrder -Value $SuffixSearchOrder
+        $indResult | Add-Member -MemberType NoteProperty -Name SuffixRegistration -Value $SuffixRegistration
+        $indResult | Add-Member -MemberType NoteProperty -Name Description -Value $Description           
+        $EndindResult += $indResult
+
+        }
    }
    else 
         { Write-Warning "I could not connect to $ind using Test-WSMan, please investigate this manually" }
 }
-#Super lazy here outputting to csv gave me System.String[] since you could have multiple IP's or subnets on a nic
-#to avoid this problem i just output to table then into a file in txt and manipulate it in excel
-$networks | ft > C:\subnet_results.txt
 
-# Output results to console at the end
-$networks 
+$EndindResult | ft -autosize
